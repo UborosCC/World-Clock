5fa5ba9ea909bf933768799318b9c5073bfbcc82
Alex Vong <avong4@gmail.com>
2025-09-12 09:08:32 +0200

    change link path

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,8 +5,8 @@ export default function Navbar() {
     return(
         <nav className="navbar">
             {/* Navbar links */}
-            <Link to="/" className="navbar-link">Home</Link>
-            <Link to="/AddTimezone" className="navbar-link">Add your own timezone</Link>
+            <Link to="./" className="navbar-link">Home</Link>
+            <Link to="./AddTimezone" className="navbar-link">Add your own timezone</Link>
         </nav>
     )
 }
